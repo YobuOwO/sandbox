9bc456f9f45458fc55e7d42033918b6eb3b425af
yticzon <y.ticzon@gmail.com>
2021-07-07 06:57:22 +0800

    fixed date

--- js/general.js
+++ js/general.js
@@ -1,5 +1,4 @@
 function showDate() {
     var date = Date();
-    document.getElementById("date").innerHTML(date + "<br>");
-    alert(date);
+    document.getElementById("date").innerHTML = date + "<br>";
 }
